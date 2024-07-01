d5caca463bba76cffa5ba85f3c6852fa2cb706e1
Shoptrade <shoptrade@Shoptrades-MacBook-Pro.local>
2024-07-01 15:52:49 +0530

    code for change title

---
+++ extensions/opp-variant-function/src/run.js
@@ -0,0 +1,36 @@
+// @ts-check
+
+/**
+ * @typedef {import("../generated/api").RunInput} RunInput
+ * @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
+ * @typedef {import("../generated/api").CartOperation} CartOperation
+ */
+
+/**
+ * @type {FunctionRunResult}
+ */
+const NO_CHANGES = {
+  operations: [],
+};
+
+/**
+ * @param {RunInput} input
+ * @returns {FunctionRunResult}
+ */
+export function run(input) {
+  const operations = input.cart.lines
+    .filter(line => 
+      line.merchandise.__typename === "ProductVariant" &&
+      line.merchandise.product.title.includes(":")
+    )
+    .map(line => {
+      return {
+        update: {
+          cartLineId: line.id,
+          title: line.merchandise.product.title.split(":")[0],
+        },
+      };
+    });
+
+  return operations.length > 0 ? { operations } : NO_CHANGES;
+}
